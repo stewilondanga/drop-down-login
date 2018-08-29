a65192b8825a3f34e7debc3744090eaba11cb7b3
Stewart ilondanga <stilondanga@gmail.com>
2018-08-29 15:43:16 +0300

    add js/scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,13 @@
-/*var navigate = (function() {
-	$('.dd').toggle();
-	$('.dd_btn').click(function() {
-		var dataName = $(this).attr('data-name');
-		$('.dd').hide();
-		$('.' + dataName).toggle();
-	});
+var navigate = (function() {
+  $('.dd').toggle();
+  $('.dd_btn').click(function() {
+    var dataName = $(this).attr('data-name');
+    $('.dd').hide();
+    $('.' + dataName).toggle();
+  });
 })();
 
-var cta = document.querySelector(".cta");
+/*var cta = document.querySelector(".cta");
 var check = 0;
 
 cta.addEventListener('click', function(e){
