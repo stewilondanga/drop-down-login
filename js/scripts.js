a5aca3a2083e2dbaecdc2e78a4d86864973cd479
Stewart ilondanga <stilondanga@gmail.com>
2018-08-29 15:45:10 +0300

    add js/scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -15,15 +15,14 @@ cta.addEventListener('click', function(e) {
       var loginText = e.target.parentElement;
       text.classList.toggle('show-hide');
       loginText.classList.toggle('expand');
-      if (check == 0)
-      /*    {
-              cta.innerHTML = "<i class=\"fas fa-chevron-up\"></i>";
-              check++;
-          }
-          else
-          {
-              cta.innerHTML = "<i class=\"fas fa-chevron-down\"></i>";
-              check = 0;
-          }
-      })
-      */
+      if (check == 0) {
+        cta.innerHTML = "<i class=\"fas fa-chevron-up\"></i>";
+        check++;
+      }
+      /*          else
+                {
+                    cta.innerHTML = "<i class=\"fas fa-chevron-down\"></i>";
+                    check = 0;
+                }
+            })
+            */
